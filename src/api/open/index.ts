22dfd210a8fdbf05d15f660e234de090bc0507a0
ckkeno <ckkeno@gmail.com>
2023-02-27 20:55:09 +0800

    feat：添加axios封装

---
+++ src/api/open/index.ts
@@ -0,0 +1,9 @@
+import {http} from '@/utils/http'
+
+enum OpenApi {
+  OneSentence = ''
+}
+
+/**
+ * @description 一言开放接口
+ */
---
+++ src/utils/http.ts
@@ -0,0 +1,34 @@
+import axios from 'axios'
+import type { InternalAxiosRequestConfig, AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios'
+
+const baseURL = 'http://localhost:7001'
+
+const instance = axios.create({
+  baseURL,
+  timeout: 60000,
+})
+
+instance.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    return config
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error)
+  }
+)
+
+instance.interceptors.response.use((response: AxiosResponse) => {
+  const { code, data, msg } = response.data
+
+  return Promise.resolve(response)
+})
+
+export const http = {
+  get<T = any>(url: string, config?: AxiosRequestConfig): Promise<T> {
+    return instance.get(url, config)
+  },
+
+  post<T = any>(url: string, data: object, config: AxiosRequestConfig): Promise<T> {
+    return instance.post(url, data, config)
+  },
+}
